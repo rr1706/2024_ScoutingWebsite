9b45361fe28d2f466f715d1b3a90cbe3b6ad903e
Scouting1706 <100159715+Scouting1706@users.noreply.github.com>
2025-02-20 20:01:07 -0600

    2025 View Scout Notes MatchBMatch page changes

---
+++ src/TeamDetails/TeamNotesTable2025.tsx
@@ -0,0 +1,63 @@
+import Button from "../Utils/Button";
+import { useContext, useEffect, useState } from "react";
+import { matchDataDTO_2025 } from "../Utils/Utils.models";
+import { matchDataDTO } from "../TeamParser/TeamParser.model";
+import { Link } from "react-router-dom";
+import { SuperScoutDataDTO } from "../SuperScoutData/SuperScoutData.model";
+import SuperScoutData from "../SuperScoutData/SuperScoutData";
+
+export default function TeamNotesTable2025(props: tableProps) {
+    const [notes, setNotes] = useState<SuperScoutDataDTO[]>();
+    useEffect(() => {
+        mergeNotesTables()
+    }, [props.superScout, props.matchData]);
+    async function mergeNotesTables() {
+        let allNotes: SuperScoutDataDTO[] = []
+        allNotes = [...props.superScout];
+        for (var note of props.matchData) {
+            console.log('hello World')
+            let currentNote: SuperScoutDataDTO =
+            {
+                eventCode: note.eventCode,
+                matchNumber: note.matchNumber,
+                scoutName: note.scoutName,
+                teamNumber: note.teamNumber,
+                comments: note.notes,
+                type: 'normal'
+            }
+            allNotes.push(currentNote)
+            console.log(allNotes)
+        }
+        setNotes(allNotes);
+    }
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-bordered">
+                <thead>
+                    <tr className="font-weight-bold">
+                        <td className="text-center align-middle"><b>Match</b></td>
+                        <td className="text-center align-middle"><b>Scout</b></td>
+                        <td className="text-center align-middle"><b>Type</b></td>
+                        <td className="text-center align-middle"><b>Notes</b></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {notes?.map((thisItem, index) => (
+                        <tr key={index}>
+                            <td className="text-center align-middle" >{thisItem.matchNumber}</td>
+                            <td className="text-center align-middle" >{thisItem.scoutName}</td>
+                            {/*broke*/}
+                            <td className="text-center align-middle" >{thisItem.type}</td>
+                            <td className="text-center align-middle" >{thisItem.comments}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+interface tableProps {
+    matchData: matchDataDTO_2025[];
+    superScout: SuperScoutDataDTO[];
+}
